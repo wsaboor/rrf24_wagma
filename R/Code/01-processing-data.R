c0a7bee6c991aca3a6c18a572a726a02896c5de7
Wagma <49102509+wsaboor@users.noreply.github.com>
2024-10-01 13:04:50 -0400

    Day_1 Processing - practice

--- R/Code/01-processing-data.R
+++ R/Code/01-processing-data.R
@@ -2,40 +2,50 @@
 # 01. Data processing
 
 ### Libraries
-# library(haven)
-# library(dplyr)
-# library(tidyr)
-# library(stringr)
-# library(labelled)
+library(haven)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(labelled)
 
 ### Loading data ----
 
 # Load the dataset
-data_path <- "ADD-YOUR-PATH"
+data_path <- "C:/Users/wb610097/Downloads/DataWork/DataWork/Data/"
 data      <- read_dta(file.path(data_path, "Raw/TZA_CCT_baseline.dta"))
 
-### Remove duplicates based on hhid
-data_dedup <- data %>%
-    ......
-
+## we reviewed the duplicates in the data and we found duplicates in hhid variabales.
+### Remove duplicates based on hhidds
+ ##data_dedup <- data %>%
+    data_clean <- data %>%
+        distinct(hhid, .keep_all = TRUE)
+    
 ### Household (HH) level data ----
 
 #### Tidying data for HH level
-data_tidy_hh <- data_dedup %>%
-    ......
+data_tidy_hh <- data_clean %>%
+        select(vid, hhid, enid, floor:n_elder, food_cons: submissionday) 
 
 ### Data cleaning for Household-member (HH-member) level
 data_clean_hh <- data_tidy_hh %>%
     # Convert submissionday to date
-    mutate(...... = as.Date(......, format = "%Y-%m-%d %H:%M:%S")) %>%
+    mutate(submissiondate = as.Date(submissionday, format = "%Y-%m-%d %H:%M:%S")) %>%
     # Convert duration to numeric (if it is not already)
-    mutate(......) %>%
+    mutate(duration) %>%
     # Convert ar_farm_unit to factor (categorical data)
-    mutate(......) %>%
+    mutate(ar_unit = as.numeric(duration))%>%
+    mutate(ar_unit = as.factor (ar_farm_unit))
+    mutate(ar_unit = na_if(ar_unit, ""))
+    mutate(crop_other = str_to_title(crop_other)) 
+  
     # Replace values in the crop variable based on crop_other using regex for new crops
-    mutate(crop = case_when(
-        ......
+    mutate(crop = case_when( 
+        str_detect(crop_other, "coconut") ~ 40,
+        str_detect(crop_other, "sesame")~41,
+        TRUE ~crop
     )) %>%
+        
+
     # Recode negative numeric values (-88) as missing (NA)
     mutate(across(......)) %>%
     # Add variable labels
@@ -49,11 +59,14 @@ write_dta(data_clean_hh, file.path(data_path, "Intermediate/TZA_CCT_HH.dta"))
 ### Household member (HH-member) level data ----
 
 #### Tidying data for HH-member level
-data_tidy_mem <- data_dedup %>%
-    select(......,
-           starts_with(......)) %>%
+data_tidy_mem <- data_clean %>%
+    select(vid, hhid,enid,starts_with("gender"), starts_with("age"),
+           starts_with("read"), starts_with("clinic_visit"), starts_with("sick"),
+           starts_with("days_sick"), starts_with("treat-fin"),
+           starts_with("treat_cost"), starts_with("ill_impact"),
+           starts_with("days_impact")) %>%
     pivot_longer(cols = -c(vid, hhid, enid),  # Keep IDs static
-                 names_to = ......,
+                 names_to = c(".value", "member"),
                  names_pattern = "(.*)_(\\d+)")  # Capture the variable and the suffix
 
 ### Data cleaning for HH-member level
@@ -73,9 +86,10 @@ secondary_data <- read.csv(file.path(data_path, "Raw/TZA_amenity.csv"))
 
 # Tidying data
 secondary_data <- secondary_data %>%
-    pivot_wider(names_from = ......,
-                values_from = ......,
-                names_prefix = ......)
+    pivot_wider(names_from = amenity,
+                values_from = n,
+                names_prefix = "n_")
 
 # Save the final tidy secondary data
 write_dta(secondary_data, file.path(data_path, "Intermediate/TZA_amenity_tidy.dta"))
+
